0f93ffd658db3de9a53d63e09558138b66f913c7
YOOHYUNJUN <rnlayrnlay@gmail.com>
2024-07-21 21:03:59 +0900

    test

--- 개인 학습/프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public int solution(int num1, int num2) {
-        float num3 = (float) num1 / num2 * 1000;
-        int answer = (int) num3;
-        
-        return answer;
-    }
-}
